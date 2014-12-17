40ff477a267839eb2d30af2f276756bb7f1ece9f
Marco Paulo <markoskt@gmail.com>
2014-12-16 22:30:49 -0200

    chore: Include the watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	watch = require('gulp-watch'),
+	plumber = require('gulp-plumber');
 
 gulp.task('concat', function() {
   gulp.src(['assets/js/app/core/**/*.js','assets/js/app/*.js'])
@@ -12,4 +14,9 @@ gulp.task('compress', function() {
   gulp.src('assets/js/min/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('assets/js/min/'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('assets/js/app/**/*.js', ['concat']);
+  gulp.watch('assets/js/min/*.js', ['compress']);
 });
